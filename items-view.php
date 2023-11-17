b825183971e1868a02d75ddb2379419d741269a2
Arshad Jasir <132915436+Arshadjasir@users.noreply.github.com>
2023-11-17 17:03:25 +0530

    12

--- items-view.php
+++ items-view.php
@@ -10,7 +10,7 @@ $dbname = "vb_project";
 $conn = new mysqli($servername, $username, $password, $dbname);
 $result=mysqli_query($conn,"SELECT * FROM items");
 $data = array();
-
+$summa = 00;
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $data[] = $row;
