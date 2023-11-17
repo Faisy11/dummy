b6f290df84cbdfc39532b83e425338c2e65c5560
FAYAS <132914457+Faisy11@users.noreply.github.com>
2023-11-17 15:18:30 +0400

    shg

---
+++ items-view.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "vb_project";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$result=mysqli_query($conn,"SELECT * FROM items");
+$data = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+echo json_encode($data);
+?>
